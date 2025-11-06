cadf6fe400608194c2dd8bf24a373944c24b34f0
sarnicola <fsarnicola@udesa.edu.ar>
2025-11-06 17:11:04 -0300

    profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,28 @@
-import { Text, View } from 'react-native'
+import { Text, View, Pressable } from 'react-native'
 import React, { Component } from 'react'
+import { auth } from '../firebase/Config'
+
+
 
 export default class Profile extends Component {
+
+  constructor(props){
+    super(props)
+}
+
+  logout(){
+    auth.signOut()
+  }
+
   render() {
     return (
       <View>
-        <Text>Profile</Text>
+        <Text>Soy el Usuario: {this.props.route.params.id}</Text>
+        <Text>Email del Usuario: {this.props.email}</Text>
+        
+        <Pressable onPress={()=> this.logout()}>
+          <Text>Log Out</Text>
+        </Pressable>
       </View>
     )
   }
