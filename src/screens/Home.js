3f13dc9814d954bf762ae605c3a6d9e97b820f16
sarnicola <fsarnicola@udesa.edu.ar>
2025-11-10 09:07:19 -0300

    home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,48 @@
-import { Text, View } from 'react-native'
+import { Text, View, FlatList} from 'react-native'
 import React, { Component } from 'react'
 import { db, auth } from '../firebase/Config'
+import Post from '../components/Post'
 
 export default class Home extends Component {
+
     constructor(props){
       super(props)
+      this.state = {
+        postsRecuperados: '',
+        posteos: '',
+        loading: true
+      }
     }
+
+    componentDidMount(){
+    db.collection('posts')
+    .orderBy('createdAt', 'desc')
+    .onSnapshot((docs) => {
+        let posts = [];
+        docs.forEach ((doc) => {
+          posts.push({
+            id: doc.id,
+            data: doc.data()
+          })
+        this.setState({
+          posteos: posts,
+          loading: false
+        })
+        })
+      })
+    }
+    
 
   render() {
     return (
       <View>
         <Text>Home</Text>
+        <FlatList 
+          data={this.state.postsRecuperados}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={( {item} ) => <Post data={item.data} id={item.id}/> }
+        />
+
       </View>
     )
   }
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,6 @@
 import { Text, View, Pressable } from 'react-native'
 import React, { Component } from 'react'
-import { auth } from '../firebase/Config'
+import { auth, db } from '../firebase/Config'
 
 
 
